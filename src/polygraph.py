604640971bdb253f445128183a109fa5747185b2
Arjun <arjun.narayanan91@gmail.com>
2023-10-27 15:17:21 -0700

    added tests for double hex graph

--- src/polygraph.py
+++ src/polygraph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import itertools
 
 
 class PolyGraph(nx.DiGraph):
@@ -24,7 +25,7 @@ class PolyGraph(nx.DiGraph):
         face_ids = [(idx, self.face_tag) for idx in range(self.num_faces)]
         self.add_nodes_from(face_ids, type="face")
 
-    def add_face_loop(self, source_half_edges, next_half_edges):
+    def _add_face_loop(self, source_half_edges, next_half_edges):
         assert len(source_half_edges) == len(next_half_edges)
         assert next_half_edges[-1] == source_half_edges[0]
 
@@ -36,12 +37,23 @@ class PolyGraph(nx.DiGraph):
         prev_edges = zip(next_edges_with_tags, source_edges_with_tags)
         self.add_edges_from(prev_edges, type="previous")
 
+    def add_face_loop(self, face_loop):
+        next_indices = face_loop[1:] + [face_loop[0]]
+        self._add_face_loop(face_loop, next_indices)
+
     def add_sequential_face_loop(self, halfedge_start, halfedge_stop):
         # !ASSUMES THAT HALF EDGES IN A FACE LOOP ARE INDEXED SEQUENTIALLY!
         assert halfedge_stop - halfedge_start + 1 >= 3
         source_indices = list(range(halfedge_start, halfedge_stop + 1))
         next_indices = source_indices[1:] + [source_indices[0]]
-        self.add_face_loop(source_indices, next_indices)
+        self._add_face_loop(source_indices, next_indices)
+
+    def initialize_half_edges_from_face_loops(self, face_loops):
+        start = 0
+        for loop in face_loops:
+            stop = start + len(loop) - 1
+            self.add_sequential_face_loop(start, stop)
+            start = stop + 1
 
     def add_undirected_edges(self, source, dst, name):
         # adds edges from source to dst and vice verse
@@ -49,94 +61,134 @@ class PolyGraph(nx.DiGraph):
         self.add_edges_from(zip(source, dst), type=name)
         self.add_edges_from(zip(dst, source), type=name)
 
-    def add_halfedge_to_vertex_edges(self, vertex_connectivity):
-        # ASSUMES that rows of vertex_connectivity correspond to half edges indexed in order
-        # this will not be true after the graph has been manipulated so make sure you only use this
-        # when the graph is initialized
-        # TODO : add a flag to check if the graph is in initial state
+    def add_edges_to_halfedge_source(self, face_loops):
+        source_vertices = [(v, self.vertex_tag) for v in itertools.chain.from_iterable(face_loops)]
+        halfedge_ids = [(h, self.halfedge_tag) for h in range(len(source_vertices))]
+        self.add_undirected_edges(halfedge_ids, source_vertices, "source")
 
-        assert vertex_connectivity.shape == (self.num_halfedges, 2)
+    def add_edges_to_halfedge_target(self, face_loops):
+        target_vertices = []
+        for loop in face_loops:
+            rotated_loop = loop[1:] + [loop[0]]
+            target_vertices += rotated_loop
 
-        halfedge_ids = [(idx, self.halfedge_tag) for idx in range(self.num_halfedges)]
+        target_vertex_ids = [(v, self.vertex_tag) for v in target_vertices]
+        halfedge_ids = [(h, self.halfedge_tag) for h in range(len(target_vertex_ids))]
+        self.add_undirected_edges(halfedge_ids, target_vertex_ids, "target")
 
-        source_vertex_ids = [(idx, self.vertex_tag) for idx in vertex_connectivity[:, 0]]
-        self.add_undirected_edges(halfedge_ids, source_vertex_ids, "source")
+    def add_halfedge_to_vertex_edges(self, face_loops):
+        self.add_edges_to_halfedge_source(face_loops)
+        self.add_edges_to_halfedge_target(face_loops)
 
-        dst_vertex_ids = [(idx, self.vertex_tag) for idx in vertex_connectivity[:, 1]]
-        self.add_undirected_edges(halfedge_ids, dst_vertex_ids, "target")
+    def add_halfedge_to_face_edges(self, face_loops):
+        face_ids = []
+        for face_idx, loop in enumerate(face_loops):
+            loop_face_ids = len(loop) * [face_idx]
+            face_ids += loop_face_ids
 
-    def add_halfedge_to_face_edges(self, halfedge_to_faces):
-        # ASSUMES that face_ids corresponds to half edges indexed in order. This will not be true after the
-        # graph has been manipulated so make sure to use this only when graph is initialized
-        # TODO : add a flag to check if the graph is in initial state
+        face_nodes = [(f, self.face_tag) for f in face_ids]
+        halfedge_nodes = [(h, self.halfedge_tag) for h in range(len(face_nodes))]
+        self.add_undirected_edges(halfedge_nodes, face_nodes, "face")
 
-        assert len(halfedge_to_faces) == self.num_halfedges
-        face_ids = [(idx, self.face_tag) for idx in halfedge_to_faces]
-        halfedge_ids = [(idx, self.halfedge_tag) for idx in range(self.num_halfedges)]
-
-        self.add_undirected_edges(face_ids, halfedge_ids, "face")
-
-    def _ensure_tag_form(self, idx, tag):
+    @staticmethod
+    def _ensure_tag_form(idx, tag):
         if isinstance(idx, int):
-            return (idx, tag)
+            return idx, tag
         else:
             assert isinstance(idx, tuple)
             assert len(idx) == 2
-            assert idx[1] == tag
+            # assert idx[1] == tag
             return idx
 
-    def add_twin_edges(self, vertex_connectivity):
-        # assumes that each column in `vertex_connectivity` represents the source-target vertices for each
-        # halfedge
-        assert vertex_connectivity.shape == (self.num_halfedges, 2)
+    def _add_twin_edges(self):
+        halfedge_nodes = [(h, self.halfedge_tag) for h in range(self.num_halfedges)]
+        src_target = [(self.source_vertex(h), self.target_vertex(h)) for h in halfedge_nodes]
+        src_target_to_halfedge = dict(zip(src_target, halfedge_nodes))
+        twins = []
+        boundary_nodes = []
+        boundary_count = 0
 
-        # sort the edges by vertex ids
-        sorted_connectivity = np.sort(vertex_connectivity, axis=1)
+        for idx in range(len(src_target)):
+            src, target = src_target[idx]
+            if (target, src) in src_target_to_halfedge:
+                twin_edge = src_target_to_halfedge[(target, src)]
+                twins.append(twin_edge)
+            else:
+                boundary_node = (boundary_count, self.boundary_tag)
+                boundary_nodes.append(boundary_node)
+                twins.append(boundary_node)
+                boundary_count += 1
 
-        # get the indices to sort the edges lexicographically, this sorts twin edges to be next to each other
-        sort_index = np.lexsort((sorted_connectivity[:, 1], sorted_connectivity[:, 0]))
+        self.num_boundary = boundary_count
+        self.add_nodes_from(boundary_nodes)
+        self.add_undirected_edges(halfedge_nodes, twins, "twin")
 
-        # get the connectivity in sorted order
-        sorted_connectivity = sorted_connectivity[sort_index, :]
-        # shift the connectivity and compare it to find edges that have duplicates
-        # these are twin edges
-        shifted_connectivity = np.roll(sorted_connectivity, -1, axis=0)
-        has_twin = (sorted_connectivity == shifted_connectivity).all(axis=1)
+    def _add_twin_source_target_edges(self):
+        boundary_nodes = [node for node in self.nodes() if node[1] == self.boundary_tag]
+        boundary_source = []
+        boundary_target = []
+        for bnode in boundary_nodes:
+            twin_edge = self.twin_halfedge(bnode)
+            twin_source = self.source_vertex(twin_edge)
+            twin_target = self.target_vertex(twin_edge)
+            boundary_source.append(twin_target)
+            boundary_target.append(twin_source)
 
-        # get the half edge indices in sorted order
-        half_edge_ids = np.arange(self.num_halfedges)[sort_index]
+        self.add_undirected_edges(boundary_nodes, boundary_source, "source")
+        self.add_undirected_edges(boundary_nodes, boundary_target, "target")
 
-        # associate half-edges to their twins
-        src_halfedge = half_edge_ids[has_twin]
-        dst_halfedge = half_edge_ids[np.roll(has_twin, 1)]
-
-        # add twin edges between these half edges
-        src_halfedge_verts = [(idx, self.halfedge_tag) for idx in src_halfedge]
-        dst_halfedge_verts = [(idx, self.halfedge_tag) for idx in dst_halfedge]
-        self.add_undirected_edges(src_halfedge_verts, dst_halfedge_verts, "twin")
-
-        # find all boundary edges which do not have twins
-        # make sure you select ALL indices that have twins -- for this you need to roll forward
-        # the has_twin vector
-        _has_twin = np.logical_or(has_twin, np.roll(has_twin, +1))
-        is_boundary = ~_has_twin
-        self.num_boundary = np.count_nonzero(is_boundary)
-        # create num_boundary boundary half edges
-        boundary_node_ids = [(idx, self.boundary_tag) for idx in range(self.num_boundary)]
-        self.add_nodes_from(boundary_node_ids, type="boundary")
-
-        # add twin edges between half edges and boundary nodes
-        boundary_halfedge_ids = half_edge_ids[is_boundary]
-        boundary_halfedge_verts = [(idx, self.halfedge_tag) for idx in boundary_halfedge_ids]
-        self.add_undirected_edges(boundary_halfedge_verts, boundary_node_ids, name="twin")
-
-        # target vertices of half edges on boundary are source vertices of boundary nodes
-        boundary_src_nodes = [self.target_vertex(half_edge_idx) for half_edge_idx in boundary_halfedge_verts]
-        self.add_undirected_edges(boundary_node_ids, boundary_src_nodes, name="source")
-
-        # source vertices of half edges on boundary are target vertices of boundary nodes
-        boundary_target_nodes = [self.source_vertex(half_edge_idx) for half_edge_idx in boundary_halfedge_verts]
-        self.add_undirected_edges(boundary_node_ids, boundary_target_nodes, name="target")
+    # def add_twin_edges(self, vertex_connectivity):
+    #     # assumes that each column in `vertex_connectivity` represents the source-target vertices for each
+    #     # halfedge
+    #     assert vertex_connectivity.shape == (self.num_halfedges, 2)
+    #
+    #     # sort the edges by vertex ids
+    #     sorted_connectivity = np.sort(vertex_connectivity, axis=1)
+    #
+    #     # get the indices to sort the edges lexicographically, this sorts twin edges to be next to each other
+    #     sort_index = np.lexsort((sorted_connectivity[:, 1], sorted_connectivity[:, 0]))
+    #
+    #     # get the connectivity in sorted order
+    #     sorted_connectivity = sorted_connectivity[sort_index, :]
+    #     # shift the connectivity and compare it to find edges that have duplicates
+    #     # these are twin edges
+    #     shifted_connectivity = np.roll(sorted_connectivity, -1, axis=0)
+    #     has_twin = (sorted_connectivity == shifted_connectivity).all(axis=1)
+    #
+    #     # get the half edge indices in sorted order
+    #     half_edge_ids = np.arange(self.num_halfedges)[sort_index]
+    #
+    #     # associate half-edges to their twins
+    #     src_halfedge = half_edge_ids[has_twin]
+    #     dst_halfedge = half_edge_ids[np.roll(has_twin, 1)]
+    #
+    #     # add twin edges between these half edges
+    #     src_halfedge_verts = [(idx, self.halfedge_tag) for idx in src_halfedge]
+    #     dst_halfedge_verts = [(idx, self.halfedge_tag) for idx in dst_halfedge]
+    #     self.add_undirected_edges(src_halfedge_verts, dst_halfedge_verts, "twin")
+    #
+    #     # find all boundary edges which do not have twins
+    #     # make sure you select ALL indices that have twins -- for this you need to roll forward
+    #     # the has_twin vector
+    #     _has_twin = np.logical_or(has_twin, np.roll(has_twin, +1))
+    #     is_boundary = ~_has_twin
+    #     self.num_boundary = np.count_nonzero(is_boundary)
+    #     # create num_boundary boundary half edges
+    #     boundary_node_ids = [(idx, self.boundary_tag) for idx in range(self.num_boundary)]
+    #     self.add_nodes_from(boundary_node_ids, type="boundary")
+    #
+    #     # add twin edges between half edges and boundary nodes
+    #     boundary_halfedge_ids = half_edge_ids[is_boundary]
+    #     boundary_halfedge_verts = [(idx, self.halfedge_tag) for idx in boundary_halfedge_ids]
+    #     self.add_undirected_edges(boundary_halfedge_verts, boundary_node_ids, name="twin")
+    #
+    #     # target vertices of half edges on boundary are source vertices of boundary nodes
+    #     boundary_src_nodes = [self.target_vertex(half_edge_idx) for half_edge_idx in boundary_halfedge_verts]
+    #     self.add_undirected_edges(boundary_node_ids, boundary_src_nodes, name="source")
+    #
+    #     # source vertices of half edges on boundary are target vertices of boundary nodes
+    #     boundary_target_nodes = [self.source_vertex(half_edge_idx) for half_edge_idx in boundary_halfedge_verts]
+    #     self.add_undirected_edges(boundary_node_ids, boundary_target_nodes, name="target")
 
     def source_vertex(self, halfedge_index):
         halfedge_index = self._ensure_tag_form(halfedge_index, self.halfedge_tag)
@@ -151,4 +203,23 @@ class PolyGraph(nx.DiGraph):
         twin = next(target for src, target, data in self.edges(halfedge_index, data=True) if data.get("type") == "twin")
         return twin
 
+    def next_halfedge(self, halfedge_index):
+        halfedge_index = self._ensure_tag_form(halfedge_index, self.halfedge_tag)
+        next_edge = next(
+            target for src, target, data in self.edges(halfedge_index, data=True) if data.get("type") == "next"
+        )
+        return next_edge
 
+    def previous_halfedge(self, halfedge_index):
+        halfedge_index = self._ensure_tag_form(halfedge_index, self.halfedge_tag)
+        prev_edge = next(
+            target for src, target, data in self.edges(halfedge_index, data=True) if data.get("type") == "previous"
+        )
+        return prev_edge
+
+    def face(self, halfedge_index):
+        halfedge_index = self._ensure_tag_form(halfedge_index, self.halfedge_tag)
+        face = next(
+            target for src, target, data in self.edges(halfedge_index, data=True) if data.get("type") == "face"
+        )
+        return face
--- test/test_node_filter.py
+++ test/test_node_filter.py
@@ -4,28 +4,144 @@ import numpy as np
 import itertools
 
 
-face_loops = [
-    [0,1,2,7,8,9],
-    [2,3,4,5,6,7]
-]
+def initialize_double_hex_graph():
+    face_loops = [
+        [0, 1, 2, 7, 8, 9],
+        [2, 3, 4, 5, 6, 7]
+    ]
 
-num_faces = len(face_loops)
-num_half_edges = sum((len(l) for l in face_loops))
-vertex_ids = set(itertools.chain.from_iterable(face_loops))
-num_vertices = len(vertex_ids)
+    num_faces = len(face_loops)
+    num_half_edges = sum((len(l) for l in face_loops))
+    vertex_ids = set(itertools.chain.from_iterable(face_loops))
+    num_vertices = len(vertex_ids)
+
+    graph = PolyGraph(num_half_edges, num_vertices, num_faces)
+    graph.initialize_half_edges_from_face_loops(face_loops)
+    graph.add_halfedge_to_vertex_edges(face_loops)
+    graph.add_halfedge_to_face_edges(face_loops)
+    graph._add_twin_edges()
+    graph._add_twin_source_target_edges()
+    return graph
 
 
-# vertex_connectivity = np.array(
-#     [[0, 1], [1, 2], [2, 7], [7, 8], [8, 9], [9, 0], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 2]]
-# )
-# face_ids = np.array(6 * [0] + 6 * [1])
-#
-# graph = PolyGraph(num_half_edges, num_vertices, num_faces)
-# graph.add_sequential_face_loop(6, 11)
-# graph.add_sequential_face_loop(0, 5)
-# graph.add_halfedge_to_vertex_edges(vertex_connectivity)
-# graph.add_halfedge_to_face_edges(face_ids)
-# graph.add_twin_edges(vertex_connectivity)
-#
-#
-# sources = [graph.source_vertex(idx) for idx in range(12)]
+class TestHalfEdgeConnectivity(unittest.TestCase):
+    def setUp(self) -> None:
+        self.graph = initialize_double_hex_graph()
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.graph.edges()), 158)
+
+        next_edges = [(src, dst) for src, dst, data in self.graph.edges(data=True) if data["type"] == "next"]
+        self.assertEqual(len(next_edges), 12)
+
+        previous_edges = [(src, dst) for src, dst, data in self.graph.edges(data=True) if data["type"] == "previous"]
+        self.assertEqual(len(previous_edges), 12)
+
+        source_edges = [(src, dst) for src, dst, data in self.graph.edges(data=True) if data["type"] == "source"]
+        self.assertEqual(len(source_edges), 44)
+
+        target_edges = [(src, dst) for src, dst, data in self.graph.edges(data=True) if data["type"] == "target"]
+        self.assertEqual(len(target_edges), 44)
+
+        face_edges = [(src, dst) for src, dst, data in self.graph.edges(data=True) if data["type"] == "face"]
+        self.assertEqual(len(face_edges), 24)
+
+    def test_num_graph_nodes(self):
+        num_halfedge = len([v for v, data in self.graph.nodes(data=True) if data.get("type") == "halfedge"])
+        self.assertEqual(num_halfedge, 12)
+
+        num_verts = len([v for v, data in self.graph.nodes(data=True) if data.get("type") == "vertex"])
+        self.assertEqual(num_verts, 10)
+
+        num_faces = len([v for v, data in self.graph.nodes(data=True) if data.get("type") == "face"])
+        self.assertEqual(num_faces, 2)
+
+        num_boundary = len([v for v in self.graph.nodes() if v[1] == self.graph.boundary_tag])
+        self.assertEqual(num_boundary, 10)
+
+        total_num_nodes = self.graph.number_of_nodes()
+        self.assertEqual(total_num_nodes, 34)
+
+    def test_next_edges(self):
+        next_edges = list(range(1, 6)) + [0]
+        self.assertTrue(
+            all(self.graph.next_halfedge(idx) == (ne, self.graph.halfedge_tag) for idx, ne in zip(range(6), next_edges))
+        )
+        next_edges = list(range(7, 12)) + [6]
+        self.assertTrue(
+            all(self.graph.next_halfedge(idx) == (ne, self.graph.halfedge_tag) for idx, ne in
+                zip(range(6, 12), next_edges))
+        )
+
+    def test_previous_edges(self):
+        next_edges = list(range(1, 6)) + [0]
+        self.assertTrue(
+            all(self.graph.previous_halfedge(ne) == (idx, self.graph.halfedge_tag) for idx, ne in
+                zip(range(6), next_edges))
+        )
+        next_edges = list(range(7, 12)) + [6]
+        self.assertTrue(
+            all(
+                self.graph.previous_halfedge(ne) == (idx, self.graph.halfedge_tag) for idx, ne in
+                zip(range(6, 12), next_edges)
+            )
+        )
+
+    def test_source_edges(self):
+        source_vertices = [0, 1, 2, 7, 8, 9, 2, 3, 4, 5, 6, 7]
+        self.assertTrue(
+            all(self.graph.source_vertex(h) == (v, self.graph.vertex_tag) for (h, v) in
+                zip(range(12), source_vertices))
+        )
+
+        vertex_ids = [0, 1, 2, 2, 3, 4, 5, 6, 7, 7, 8, 9]
+        source_halfedge_ids = [0, 1, 2, 6, 7, 8, 9, 10, 11, 3, 4, 5]
+        self.assertTrue(
+            all(
+                self.graph.has_edge((v, self.graph.vertex_tag), (h, self.graph.halfedge_tag)) for (v, h) in
+                zip(vertex_ids, source_halfedge_ids)
+            )
+        )
+
+    def test_target_edges(self):
+        vertex_ids = [0, 1, 2, 2, 3, 4, 5, 6, 7, 7, 8, 9]
+        target_halfedge_ids = [5, 0, 1, 11, 6, 7, 8, 9, 10, 2, 3, 4]
+        self.assertTrue(
+            all(
+                self.graph.has_edge((v, self.graph.vertex_tag), (h, self.graph.halfedge_tag)) for (v, h) in
+                zip(vertex_ids, target_halfedge_ids)
+            )
+        )
+
+    def test_face_edges(self):
+        face_ids = 6 * [0] + 6 * [1]
+        self.assertTrue(
+            all(
+                self.graph.face(halfedge) == (face, self.graph.face_tag) for halfedge, face in zip(range(12), face_ids)
+            )
+        )
+
+        self.assertTrue(
+            all(
+                self.graph.has_edge((f, self.graph.face_tag), (h, self.graph.halfedge_tag))
+                for (f, h) in zip(face_ids, range(12))
+            )
+        )
+
+    def test_twin_edges(self):
+        twin_id = [0, 1, 11, 2, 3, 4, 5, 6, 7, 8, 9, 2]
+        htag = self.graph.halfedge_tag
+        btag = self.graph.boundary_tag
+        twin_tags = 2 * [btag] + [htag] + 8 * [btag] + [htag]
+        twin_nodes = [(node_id, node_tag) for node_id, node_tag in zip(twin_id, twin_tags)]
+        self.assertTrue(
+            all(self.graph.twin_halfedge(h) == t for h, t in zip(range(self.graph.num_halfedges), twin_nodes))
+        )
+
+
+# graph = initialize_double_hex_graph()
+# graph._add_twin_edges()
+# graph._add_twin_source_target_edges()
+
+if __name__ == "__main__":
+    unittest.main()
