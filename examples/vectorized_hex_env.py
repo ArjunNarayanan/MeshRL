60b6e1ed0e300a2c6d99efe5b41523daca40eed9
ArjunNarayanan <arjun.narayanan91@gmail.com>
2023-11-28 09:34:12 -0800

    testing longer horizon on expanse

--- examples/vectorized_hex_env.py
+++ examples/vectorized_hex_env.py
@@ -1,15 +1,21 @@
 import torch
 from stable_baselines3.common.env_util import make_vec_env
+import timeit
+import os
+import sys
+sys.path.append(os.getcwd())
 from envs.hex_env_with_insert import HexEnv
 from src.feature_extractor import FeatureExtractor
-import timeit
+
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 vec_env = make_vec_env(HexEnv, 64)
 
 obs = vec_env.reset()
 
 
-twin = torch.tensor(obs["twin"])
+twin = torch.tensor(obs["twin"]).to(device)
 
 t1 = FeatureExtractor._archive_unroll_and_offset_indices(twin)
 t2 = FeatureExtractor.unroll_and_offset_indices(twin)
@@ -26,12 +32,13 @@ twin = torch.tensor(obs["twin"])
 """
 
 
-timeit.timeit(
+t1 = timeit.timeit(
     "FeatureExtractor._archive_unroll_and_offset_indices(twin)",
     setup=setup,
     number=1000
 )
-timeit.timeit(
+
+t2 = timeit.timeit(
     "FeatureExtractor.unroll_and_offset_indices(twin)",
     setup=setup,
     number=1000
