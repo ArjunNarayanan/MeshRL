6f06b5fd9f3b12911c002876838ecfd2924e77b7
Arjun <arjun.narayanan91@gmail.com>
2023-12-05 13:58:03 -0800

    evaluating poly-20

--- examples/debug_environment_exception.py
+++ examples/debug_environment_exception.py
@@ -2,34 +2,18 @@ import pickle
 from src.polygraph import PolyGraph
 from src.render import Renderer
 
-vertex_coords = [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [2, 3],
-    [0, 3],
-    [1, 1],
-    [1.5, 1],
-    [1.5, 2],
-    [0.5, 2],
-    [0.5, 1]
-]
-vertex_coords = dict(zip(range(10), vertex_coords))
-face_loop = [0, 1, 5, 9, 8, 7, 6, 5, 1, 2, 3, 4]
-graph = PolyGraph.from_face_loops([face_loop], vertex_coordinates=vertex_coords)
-renderer = Renderer(graph, graph.vertex_coordinates)
+
+
+filename = "experiments/regular-polygon/poly-20/debug-log/except-env-1.pkl"
+with open(filename, "rb") as f:
+    data = pickle.load(f)
+
+graph = data["graph"]
+renderer = Renderer(graph, graph.vertex_coordinates, vertex_size=10)
 renderer.plot()
 
-# filename = "experiments/hex_env_with_insert/incremental-reward/except_env_0.pkl"
-# with open(filename, "rb") as f:
-#     data = pickle.load(f)
-#
-# graph = data["graph"]
-# renderer = Renderer(graph, graph.vertex_coordinates, vertex_size=10)
-# renderer.plot()
-#
-# actions = data["actions"]
-#
+actions = data["actions"]
+
 # graph.insert_halfedge(2,1)
 # renderer.plot()
 #
---
+++ examples/evaluate_regular_polygon.py
@@ -0,0 +1,92 @@
+import torch
+from stable_baselines3 import PPO
+from envs.regular_polygon_env import RegularPolygonEnv
+from src.render import Renderer
+import matplotlib.pyplot as plt
+import os
+from src.utils import load_yaml_config
+
+
+def plot_distribution(probs):
+    fig, ax = plt.subplots()
+    xtick_locs = range(len(probs))
+
+    ax.bar(xtick_locs, probs)
+    # ax.set_xticks(xtick_locs)
+    ax.set_xlabel("Actions")
+    ax.set_ylabel("Probabilities")
+    ax.grid()
+    return fig
+
+
+def initialize_environment():
+    env_config = config["environment"]
+    env = RegularPolygonEnv.from_config(env_config)
+    return env
+
+
+def get_action_distribution(obs):
+    template_halfedges = env.index_to_halfedge
+    print("Template : ", template_halfedges)
+    with torch.no_grad():
+        dist = model.policy.get_distribution(obs)
+    probs = dist.distribution.probs[0]
+    plot_distribution(probs)
+    return dist
+
+
+def step_environment(dist):
+    action = dist.get_actions()
+    action = action.cpu().numpy()
+
+    action_halfedge, action_type = env._linear_action_index_to_halfedge_and_action(action[0])
+
+    print("Selected action : ", action[0])
+    print("\tHalfedge: ", action_halfedge, " \tType: ", action_type)
+    obs, reward, done, truncated, info = env.step(action[0])
+    print("Reward : ", reward)
+    print("Terminated : ", done)
+    return obs_as_tensor(obs)
+
+
+def obs_as_tensor(obs):
+    _obs = {}
+    for k, v in obs.items():
+        v = torch.tensor(v).unsqueeze(0)
+        _obs[k] = v
+    return _obs
+
+
+def save_figure():
+    figname = "step-" + str(step) + ".png"
+    output_file = os.path.join(fig_output_folder, figname)
+    renderer.fig.savefig(output_file)
+
+
+def make_output_folder(dir):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+
+input_folder = "experiments/regular-polygon/poly-20"
+fig_output_folder = os.path.join(input_folder, "figures")
+make_output_folder(fig_output_folder)
+
+checkpoint = os.path.join(input_folder, "best_model.zip")
+model = PPO.load(checkpoint)
+
+config_fn = os.path.join(input_folder, "config.yml")
+config = load_yaml_config(config_fn)
+
+env = initialize_environment()
+obs = obs_as_tensor(env._get_obs())
+step = 0
+
+renderer = Renderer(env.graph, env.graph.vertex_coordinates, label_halfedge=True)
+renderer.plot()
+
+# dist = get_action_distribution(obs)
+# obs = step_environment(dist)
+# renderer.plot()
+# step += 1
+# save_figure()
