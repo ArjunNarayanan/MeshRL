701982201eae10caad2bf3f9fed3cd4cc38c7237
Arjun <arjun.narayanan91@gmail.com>
2024-02-15 11:05:48 -0800

    no major changes

--- debug/debug_environment_exception.py
+++ debug/debug_environment_exception.py
@@ -9,37 +9,10 @@ from envs.angle_env import AngleEnv
 from src.utils import load_yaml_config
 
 if __name__ == "__main__":
-    filename = "../debug/experiments/except-env-4.pkl"
-    with open(filename, "rb") as f:
-        data = pickle.load(f)
+    config_fn = "../experiments/angle-env/quad/face-angle-vertex/config.yml"
+    config = load_yaml_config(config_fn)
 
-    graph = data["graph"]
-    env = AngleEnv(3, [5])
-    env.graph = graph
-    env._update_scores_on_reset()
-    env._set_half_edge_template_center(env.graph.half_edge_list())
-    env._build_template()
+    env_config = config["environment"]
+    env = AngleEnv.from_config(env_config)
 
-    actions = data["actions"]
-    # renderer = Renderer(env.graph, env.graph.vertex_coordinates, label_halfedge=True)
-    # renderer.plot()
-
-    for idx, data in enumerate(actions):
-        # renderer = Renderer(env.graph, env.graph.vertex_coordinates, label_halfedge=True)
-        # renderer.plot()
-
-        hidx = data[0]
-        action = data[1]
-        print("idx : ", idx, "\thidx : ", hidx, "\taction : ", action)
-        env._step_half_edge_action(hidx, action)
-        env.num_steps += 1
-
-        env._update_half_edge_template_center()
-        print("Template center : ", env.template_center)
-        env._build_template()
-        env.terminated = env.is_terminated()
-        observation = env._get_obs()
-        # reward = env._get_reward()
-
-    # renderer = Renderer(env.graph, env.graph.vertex_coordinates)
-    # renderer.plot()
+    env.step(0)
