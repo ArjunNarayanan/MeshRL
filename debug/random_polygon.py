7ae9659ff15fd1faccc74474a008eb70b9b0313d
Arjun Narayanan <arjun.narayanan91@gmail.com>
2024-04-10 15:02:59 -0700

    testing average best returns callback

--- debug/random_polygon.py
+++
@@ -1,9 +0,0 @@
-from envs.environment_initializers import RandomPolygon
-from src.render import Renderer
-
-
-rpoly = RandomPolygon([10], 90)
-graph, vd = rpoly()
-
-render = Renderer(graph, graph.vertex_coordinates)
-render.plot()
---
+++ workflows/optimize_hyperparams_best_returns.py
@@ -0,0 +1,253 @@
+import argparse
+import optuna
+from optuna.pruners import MedianPruner
+from optuna.samplers import TPESampler
+from optuna.storages import JournalStorage, JournalFileStorage
+from stable_baselines3 import PPO
+from src.average_best_eval_callback import AverageBestCallback
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
+import torch
+import os
+import sys
+import datetime
+
+sys.path.append(os.getcwd())
+from envs.environment_maker import initialize_environment, get_env_feature_size
+from src.convolution_feature_extractor import ConvolutionFeatureExtractor as FeatureExtractor
+from src.policy import CustomActorCriticPolicy
+from src.utils import load_yaml_config
+
+
+def sample_ppo_params(trial: optuna.Trial):
+    """Sampler for PPO hyperparameters."""
+    gamma = 1.0
+    n_steps = 1024
+    batch_size = 512
+
+    gae_lambda = trial.suggest_float("gae_lambda", low=0.5, high=0.999, log=False)
+    ent_coef = trial.suggest_float("ent_coef", 1e-8, 0.5, log=True)
+    vf_coef = trial.suggest_float("vf_coef", 1e-8, 0.5, log=True)
+    clip_range = trial.suggest_float("clip_range", 1e-2, 0.20)
+
+    max_grad_norm = trial.suggest_float("max_grad_norm", 0.1, 5.0)
+    learning_rate = trial.suggest_float("lr", 1e-6, 0.001, log=True)
+
+    ortho_init = trial.suggest_categorical("ortho_init", [False, True])
+    feature_extractor_layers = trial.suggest_int("feature_extractor_layers", 2, 10)
+
+    feature_extractor_size_options = [2 ** x for x in range(6, 11)]
+    feature_extractor_size = trial.suggest_categorical(
+        "feature_extractor_size",
+        choices=feature_extractor_size_options
+    )
+
+    policy_kwargs = dict(
+        features_extractor_class=FeatureExtractor,
+        features_extractor_kwargs=dict(
+            input_features=get_env_feature_size(env_config),
+            output_features=feature_extractor_size,
+            number_of_layers=feature_extractor_layers
+        ),
+        ortho_init=ortho_init,
+    )
+
+    return {
+        "n_steps": n_steps,
+        "batch_size": batch_size,
+        "gamma": gamma,
+        "gae_lambda": gae_lambda,
+        "clip_range": clip_range,
+        "learning_rate": learning_rate,
+        "ent_coef": ent_coef,
+        "vf_coef": vf_coef,
+        "max_grad_norm": max_grad_norm,
+        "policy_kwargs": policy_kwargs,
+    }
+
+
+class TrialEvalCallback(AverageBestCallback):
+    """Callback used for evaluating and reporting a trial."""
+
+    def __init__(
+            self,
+            env_config,
+            trial: optuna.Trial,
+            eval_freq,
+            trials_per_env=10,
+            trials_per_eval=20,
+            deterministic: bool = False,
+            best_model_save_path=None,
+            vec_env_class=SubprocVecEnv
+    ):
+        super().__init__(
+            env_config=env_config,
+            eval_freq=eval_freq,
+            best_model_save_path=best_model_save_path,
+            vec_env_class=vec_env_class,
+            trials_per_env=trials_per_env,
+            trials_per_eval=trials_per_eval,
+            num_envs=NUM_ENVS,
+            deterministic=deterministic,
+        )
+        self.trial = trial
+        self.eval_idx = 0
+        self.is_pruned = False
+
+    def _on_step(self) -> bool:
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+            super()._on_step()
+            self.eval_idx += 1
+            self.trial.report(self.last_mean_reward, self.eval_idx)
+            # Prune trial if needed.
+            if self.trial.should_prune():
+                self.is_pruned = True
+                raise optuna.TrialPruned()
+
+        return True
+
+
+class Objective:
+    def __init__(self, gpu_id):
+        self.gpu_id = gpu_id
+
+    def __call__(self, trial):
+
+        env = make_vec_env(
+            lambda: initialize_environment(env_config),
+            NUM_ENVS,
+            vec_env_cls=SubprocVecEnv
+        )
+
+        trial_logdir = os.path.join(output_folder, "trial-" + str(trial.number))
+        DEFAULT_HYPERPARAMS = {
+            "policy": CustomActorCriticPolicy,
+            "env": env,
+            "verbose": 1,
+        }
+
+        kwargs = DEFAULT_HYPERPARAMS.copy()
+        kwargs.update(sample_ppo_params(trial))
+        kwargs["device"] = torch.device("cuda:" + str(self.gpu_id))
+        kwargs["tensorboard_log"] = trial_logdir
+
+        model = PPO(**kwargs)
+
+        eval_env = make_vec_env(
+            lambda: initialize_environment(env_config),
+            NUM_ENVS
+        )
+
+        eval_callback = TrialEvalCallback(
+            eval_env,
+            trial,
+            eval_freq=EVAL_FREQ,
+            trials_per_env=TRIALS_PER_ENV,
+            trials_per_eval=TRIALS_PER_EVAL,
+            deterministic=False,
+            vec_env_class=SubprocVecEnv
+        )
+
+        nan_encountered = False
+        try:
+            model.learn(N_TIMESTEPS, callback=eval_callback)
+        except AssertionError as e:
+            # Sometimes, random hyperparams can generate NaN.
+            print(e)
+            nan_encountered = True
+        finally:
+            # Free memory.
+            model.env.close()
+            eval_env.close()
+
+        # Tell the optimizer that the trial failed.
+        if nan_encountered:
+            return float("nan")
+
+        if eval_callback.is_pruned:
+            raise optuna.exceptions.TrialPruned()
+
+        return eval_callback.last_mean_reward
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Optimize PPO hyperparams with Optuna")
+    parser.add_argument("-num_envs", default=10, type=int)
+    parser.add_argument("-config", required=True)
+    parser.add_argument("-gpu", default=0, type=int)
+    args = parser.parse_args()
+
+    NUM_ENVS = args.num_envs
+    config_filename = args.config
+    gpu_id = args.gpu
+    config = load_yaml_config(config_filename)
+    env_config = config["environment"]
+
+    output_folder = os.path.dirname(config_filename)
+    env_config = config["environment"]
+    env_config["logdir"] = output_folder
+
+    N_TRIALS = int(config["num_trials"])
+    N_STARTUP_TRIALS = 10
+    N_EVALUATIONS = 100
+    TRIALS_PER_ENV = 10
+    TRIALS_PER_EVAL = 20
+    N_TIMESTEPS = int(config["total_timesteps"])
+
+    print("EXPERIMENT START TIMESTAMP : ", datetime.datetime.now(), "\n\n")
+
+    print("\nTotal timesteps : ", N_TIMESTEPS, "\n")
+    EVAL_FREQ = int(N_TIMESTEPS / N_EVALUATIONS)
+    print("\nEval Freq : ", EVAL_FREQ, "\n")
+    JOBID = os.environ.get("SLURM_JOB_ID")
+
+    # Set pytorch num threads to 1 for faster training.
+    # This seems to be slower when we use > 1 GPUs and SubprocVecEnv
+    # torch.set_num_threads(1)
+
+    sampler = TPESampler(n_startup_trials=N_STARTUP_TRIALS)
+    # Do not prune before 1/3 of the max budget is used.
+    pruner = MedianPruner(n_startup_trials=N_STARTUP_TRIALS, n_warmup_steps=N_EVALUATIONS // 3)
+
+    study_name = config["study_name"]
+
+    journal_file_name = study_name + ".log"
+    storage_path = os.path.join(output_folder, journal_file_name)
+
+    storage = JournalStorage(JournalFileStorage(storage_path))
+    print("\nUsing storage : ", storage_path)
+
+    study = optuna.create_study(
+        sampler=sampler,
+        pruner=pruner,
+        study_name=study_name,
+        direction="maximize",
+        storage=storage,
+        load_if_exists=True
+    )
+
+    try:
+        study.optimize(
+            Objective(gpu_id),
+            n_trials=N_TRIALS,
+            show_progress_bar=True,
+        )
+    except KeyboardInterrupt:
+        pass
+
+    print("Number of finished trials: ", len(study.trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
+
+    print("  User attrs:")
+    for key, value in trial.user_attrs.items():
+        print("    {}: {}".format(key, value))
+
+    print("EXPERIMENT END TIMESTAMP : ", datetime.datetime.now(), "\n\n")
